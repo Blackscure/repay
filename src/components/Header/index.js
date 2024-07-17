8cdd1a6d3e77d2ecac7e89d5881a60f1676e138c
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-07-17 10:25:33 +0300

    modification added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ImgLogo from "../../assets/images/img_logo.svg";
 import responsiveLogo from "../../assets/images/Union.svg";
 import strokeImage from "../../assets/images/Vector.svg";
 import searchIcon from "../../assets/images/Search.svg";
+import homeIcon from "../../assets/images/Home.svg";
+import aboutIcon from "../../assets/images/bar-chart.svg";
+import contactIcon from "../../assets/images/Document.svg";
+
+import underlineIcon from "../../assets/images/Vector 6.svg";
 import "./Header.css";
 
 export default function Header(props) {
+  const [showZenpay, setShowZenpay] = useState(false);
+
+
+  const handleStrokeImageClick = () => {
+      setShowZenpay(true);
+    };
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -20,20 +31,77 @@ export default function Header(props) {
             alt="Responsive Logo"
             className="responsive-logo"
           />
-          <img
-            src={strokeImage}
-            className="stroke-image"
-            alt="Stroke Vector Image"
-          />
+          {!showZenpay ? (
+            <img
+              src={strokeImage}
+              className="stroke-image"
+              alt="Stroke Vector Image"
+              onClick={handleStrokeImageClick}
+            />
+          ) : (
+            <span className="zenpay-text">Tipsource</span>
+          )}
         </div>
+        {showZenpay && (
+          <div className="search-container">
+            <div className="search-input">
+              <img src={searchIcon} alt="Search Icon" className="search-icon" />
+              <input
+                type="text"
+                placeholder="Search for any merchant"
+                className="search-field"
+              />
+            </div>
+            <div className="main-menu">
+              <span className="main-menu-text">Main Menu</span>
+              <div className="home-section">
+                <div className="home-icon">
+                  <img
+                    src={homeIcon}
+                    alt="Home Icon"
+                    className="home-icon-img"
+                  />
+                </div>
+                <span className="home-text">Home</span>
+              </div>
+              <div className="about-section">
+                <div className="about-icon">
+                  <img
+                    src={aboutIcon}
+                    alt="About Icon"
+                    className="about-icon-img"
+                  />
+                </div>
+                <span className="about-text">About Us</span>
+              </div>
+              <div className="contact-more-section">
+                <div className="contact-icon">
+                  <img
+                    src={contactIcon}
+                    alt="Contact Icon"
+                    className="contact-icon-img"
+                  />
+                </div>
+                <span className="contact-text">Contact Us</span>
+                <span className="more-text">More</span>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <div className="navbar-links">
-        <a href="#home" className="home-link">
-          Home
-        </a>
+        <div className="home-link-container">
+          <a href="#home" className="home-link">
+            Home
+          </a>
+          <div className="stroke">
+            <img src={underlineIcon} alt="Underline Icon" />
+          </div>
+        </div>
         <a href="#about">About Us</a>
         <a href="#contact">Contact Us</a>
       </div>
+
       <div className="navbar-search">
         <img src={searchIcon} alt="Search Icon" className="search-icon" />
         <input type="text" placeholder="Search for any merchant" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 // import { Helmet } from "react-helmet"; // Correct the spelling if you want to use this
 import Header from "../components/Header";
-import { Heading } from "../components/Heading/Index";
-import { Text } from "../components/Text"
-import { Img } from "../components/Img";
-import Footer from "../components/Footer";
-import googlePlayImage from '../assets/images/google-play.png'
+// import { Heading } from "../components/Heading/Index";
+// import { Text } from "../components/Text"
+// import { Img } from "../components/Img";
+// import Footer from "../components/Footer";
+// import googlePlayImage from '../assets/images/google-play.png'
 import ImageMaskGroup from '../assets/images/img_mask_group.png'
-import ImagVector from '../assets/images/img_vector_13.svg'
-import vector1 from '../assets/images/img_vector_1.png'
-import '../App.css'
+// import ImagVector from '../assets/images/img_vector_13.svg'
+// import vector1 from '../assets/images/img_vector_1.png'
+
+import underlineIcon from "../assets/images/Vector 6.svg";
+import googlePlayImage from "../assets/images/image 6.png";
+import appStoreImage from "../assets/images/image 7.svg";
+// import creamBackgroundImage from "../assets/images/img_vector_1.png";
+// import purpleVectorImage from "../assets/images/img_vector_13.svg";
+// import famImage from "../assets/images/two-people-using-laptop-meeting-coffee-shop 1.png";
+import './Home.css';
 
 export default function Home() {
   return (
@@ -23,7 +30,39 @@ export default function Home() {
       </Helmet> */}
       <div className="w-full bg-[#ffffff]">
         <Header />
-        
+        <div className="main-container">
+          <div className="left-section">
+            <div className="creativity-community">
+              <span className="creativity">Creativity</span>
+              <span className="meets-community">Meets Community</span>
+              <div className="stroke">
+                <img src={underlineIcon} alt="Underline Icon" />
+              </div>
+            </div>
+            <div className="creative-hub">
+              <p>
+                CreativeHub is your ultimate online destination for artists,
+                writers, musicians, designers, and all creative souls. We
+                provide a platform where your ideas can flourish, your talents
+                can shine, and your passions can connect with a like-minded
+                community.
+              </p>
+            </div>
+            <div className="download-now">
+              <p>Download now for the ultimate convenience!</p>
+              <div className="store-icons">
+                <img src={googlePlayImage} alt="Google Play" />
+                <img src={appStoreImage} alt="App Store" />
+              </div>
+            </div>
+          </div>
+          {/* <div className="right-section">
+            <div className="cream-image"></div>
+            <div className="purple-image"></div>
+            <div className="background-image"></div>
+            <div className="dashed-border"></div>
+          </div> */}
+        </div>
       </div>
     </>
   );
